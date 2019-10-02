01a9489fbe64f1b15c7a57bf5a20c9fe5627ff46
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-10-02 14:33:48 -0300

    Adding an alert dialog to the main activity

--- app/src/main/java/com/joabe/dialogforalert/MainActivity.java
+++ app/src/main/java/com/joabe/dialogforalert/MainActivity.java
@@ -1,9 +1,12 @@
 package com.joabe.dialogforalert;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +17,31 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickShowAlert(View view) {
+        AlertDialog.Builder myAlertBuilder = new
+                AlertDialog.Builder(MainActivity.this);
+
+        myAlertBuilder.setTitle("Alert");
+        myAlertBuilder.setMessage("Click Ok to continue, or Cancel to stop:");
+
+        myAlertBuilder.setPositiveButton(getString(R.string.ok_button), new
+                DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(getApplicationContext(),
+                                getString(R.string.ok_toast_message),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+        myAlertBuilder.setNegativeButton(getString(R.string.cancel_button), new
+                DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(getApplicationContext(),
+                                getString(R.string.cancel_toast_message),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        myAlertBuilder.show();
     }
 }
